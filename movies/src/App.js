e7a954a249b39d1adfabc4d3bd05bc55506a3e05
BhargaviMupparaju <105601164+BhargaviMupparaju@users.noreply.github.com>
2023-07-20 11:18:01 -0400

    Added Loading .. string using the state

--- movies/src/App.js
+++ movies/src/App.js
@@ -4,36 +4,55 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 import { useState } from "react";
+
+
 function App() {
 	const [movies, setMovies] = useState([]);
-	function fetchMoviesHandler() {
-		fetch("https://swapi.dev/api/films/")
-			.then((response) => {
-				return response.json(); // translates the data
-			})
-			.then((data) => {
-				const transformedMovies = data.results.map((movieData) => {
-					return {
-						id: movieData.episode_id,
-						title: movieData.title, // here we will put the desired name for the incoming dataresults.
-						openingText: movieData.opening_crawl,
-						releaseDate: movieData.release_date,
-					};
-				});
-				setMovies(transformedMovies);
-			});
-	}
+	const [isLoading, setIsLoading] = useState(false);
 
-	return (
-		<React.Fragment>
-			<section>
-				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
-			</section>
-			<section>
-				<MoviesList movies={movies} />
-			</section>
-		</React.Fragment>
-	);
+
+// we can use other syntax using async and await instead using the then
+  async function fetchMoviesHandler() {
+    setIsLoading(true); //Before we load the data we set it to true
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,      // here we will put the desired name for the incoming dataresults.
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);     // after loading the data we can set it to false again.
+  }
+
+	// function fetchMoviesHandler() {
+	// 	fetch("https://swapi.dev/api/films/")
+	// 		.then((response) => {
+	// 			return response.json(); // translates the data
+	// 		})
+
+	// 		.then((data) => {}
+
+
+return (
+  <React.Fragment>
+    <section>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+    </section>
+    <section>
+      {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+      {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+      {isLoading && <p>Loading...</p>}
+    </section>
+  </React.Fragment>
+);
+
 }
+
+
 
 export default App;
